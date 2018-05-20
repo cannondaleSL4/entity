012b5afc7777dcce26a0878c3388c186d7acd51c
dmitriy <workday2006@gmail.com>
2018-05-20 23:49:50 +0300

    init

--- src/main/java/com/dim/fxapp/entity/Quotes.java
+++ src/main/java/com/dim/fxapp/entity/Quotes.java
@@ -2,7 +2,9 @@ package com.dim.fxapp.entity;
 
 import com.dim.fxapp.entity.converter.LocalDateTimeConverter;
 import com.dim.fxapp.entity.enums.Period;
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -13,43 +15,39 @@ import java.time.LocalDateTime;
 /**
  * Created by dima on 28.11.17.
  */
-//@Entity
-//@Getter
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Table(name ="quotes")
 @MappedSuperclass
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name="quotes")
 @JsonTypeName("quotes")
-public abstract class Quotes implements FinancialEntity{
+public abstract class Quotes implements FinancialEntity {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @JsonProperty("id")
     private Integer id;
 
-    @Column(name="currency", nullable = false)
+    @Column(name = "currency", nullable = false)
     private String currency;
 
-    @Column(name="period", nullable = false)
+    @Column(name = "period", nullable = false)
     @Enumerated(EnumType.STRING)
     private Period period;
 
     @Column(name = "data", nullable = false)
     @JsonFormat
-        (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+            (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime data;
 
-    @Column(name="open", nullable = false)
+    @Column(name = "open", nullable = false)
     private BigDecimal open;
 
-    @Column(name="close", nullable = false)
+    @Column(name = "close", nullable = false)
     private BigDecimal close;
 
-    @Column(name="high", nullable = false)
+    @Column(name = "high", nullable = false)
     private BigDecimal high;
 
-    @Column(name="low", nullable = false)
+    @Column(name = "low", nullable = false)
     private BigDecimal low;
 }
