782895a3c606ec0bd44fda81f8235f4c99e04a93
dmitriy <workday2006@gmail.com>
2018-05-20 23:53:29 +0300

    init

--- src/main/java/com/dim/fxapp/entity/FinancialEntity.java
+++ src/main/java/com/dim/fxapp/entity/FinancialEntity.java
@@ -1,5 +1,6 @@
 package com.dim.fxapp.entity;
 
+import com.dim.fxapp.entity.impl.quotes.*;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -13,10 +14,10 @@ import java.io.Serializable;
         generator = ObjectIdGenerators.IntSequenceGenerator.class,
         property = "id"
 )
-//@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
-//        include = JsonTypeInfo.As.EXISTING_PROPERTY,
-//        property = "clazz")
-@JsonSubTypes({ @JsonSubTypes.Type(name = "quotes", value = Quotes.class) })
+@JsonSubTypes({ @JsonSubTypes.Type(name = "quotes", value = Quotes.class),
+        @JsonSubTypes.Type(name = "AUDUSD", value = AUDUSD.class),
+        @JsonSubTypes.Type(name = "EURAUD", value = EURAUD.class),
+        @JsonSubTypes.Type(name = "EURGBP", value = EURGBP.class)})
 public interface FinancialEntity extends Serializable {
 
 }
