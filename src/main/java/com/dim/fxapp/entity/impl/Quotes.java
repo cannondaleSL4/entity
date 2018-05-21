45de15d6251e33b19f16e993ef4107c1307d94e1
Dima <workday2006@yandex.ru>
2018-05-21 09:45:33 +0300

    start mongo

--- src/main/java/com/dim/fxapp/entity/impl/Quotes.java
+++ src/main/java/com/dim/fxapp/entity/impl/Quotes.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 
@@ -26,33 +27,24 @@ import java.time.LocalDateTime;
 @JsonTypeName("quotes")
 public class Quotes implements FinancialEntity{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Getter
     @JsonProperty("id")
-    private Integer id;
+    private BigInteger id;
 
-    @Column(name="currency", nullable = false)
     private String currency;
 
-    @Column(name="period", nullable = false)
     @Enumerated(EnumType.STRING)
     private Period period;
 
-    @Column(name = "data", nullable = false)
     @JsonFormat
         (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime data;
 
-    @Column(name="open", nullable = false)
     private BigDecimal open;
 
-    @Column(name="close", nullable = false)
     private BigDecimal close;
 
-    @Column(name="high", nullable = false)
     private BigDecimal high;
 
-    @Column(name="low", nullable = false)
     private BigDecimal low;
 }
